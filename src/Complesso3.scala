e8ce1b82ff659eed627ef62d29a3a70639d22c28
Luca Tosto <lucatosto1391@gmail.com>
2017-03-26 15:24:26 +0200

    Add complesso3

--- src/Complesso3.scala
+++ src/Complesso3.scala
@@ -32,8 +32,8 @@ case class Complesso3 (line: Any) {
 
   //TODO: a dinamica, intestastione flag (true o false variabili)  //(a/b)*
   println("NFA")
-  println(Tabulator.format(List(List("Stati", "a", "b", "ε", "flag"), List("0" , "/", "/", "1,7", "false"), List("1" , "/", "/", "2,3", "false"), List("2" , "4", "/","/", "false"), List("3" , "/", "5","/", "false"),List("4" , "/", "/","6", "false"),List("5" , "/", "/","6", "false"),List("6" , "/", "/","7,1", "false"),List("7" , "/", "/","/", "true") )))
+  println(Tabulator.format(List(List("Stati", "a", "b", "ε", "flag"), List("0" , "/", "/", "1,3", "false"), List("1" , "2", "/", "/", "false"), List("2" , "/", "/","3,1", "false"), List("3" , "/", "4","/", "false"),List("4" , "/", "/","/", "true"))))
   println("DFA")
-  println(Tabulator.format(List(List("Stati", "a", "b", "flag"), List("S0(0,1,2,3,7)","S1(1,2,3,4,6,7)", "S2(1,2,3,5,6,7)","true"), List("S1(1,2,3,4,6,7)", "S1(1,2,3,4,6,7)", "S2(1,2,3,5,6,7)", "true"), List("S2(1,2,3,5,6,7)", "S1(1,2,3,4,6,7)", "S2(1,2,3,5,6,7)", "true"))))
+  println(Tabulator.format(List(List("Stati", "a", "b", "flag"), List("S0(0,1,3)","S1(1,2,3)", "S2(4)","false"), List("S1(0,1,3)","S1(1,2,3)", "S2(4)","false"), List("S2(4)", "/", "/", "true"))))
 
 }
